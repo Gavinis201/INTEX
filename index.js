5d14f704925ac95ae23fc932b51a6e55540275dd
AAllred18 <161763678+AAllred18@users.noreply.github.com>
2024-12-03 14:22:48 -0700

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let app = express();
 
 let path = require("path");
 
-const port = 3000; 
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
@@ -15,8 +15,8 @@ const knex = require("knex")({
     connection: {
         host: process.env.RDS_HOSTNAME || "localhost", 
         user: process.env.RDS_USERNAME || "postgres", 
-        password: process.env.RDS_PASSWORD || "Gavin12", 
-        database: process.env.RDS_DB_NAME || "bucket_list", 
+        password: process.env.RDS_PASSWORD || "Cougars", 
+        database: process.env.RDS_DB_NAME || "TurtleShelterProject", 
         port: process.env.RDS_PORT || 5432, 
     }
 });
@@ -26,6 +26,11 @@ app.use(express.static(path.join(__dirname, "public") ))
 // Route to serve the landing page
 app.get('/', (req, res) => {
   res.render('index')
+});
+
+// Route to serve the login page
+app.get('/loginPage', (req, res) => {
+  res.render('loginPage')
 });
 
 app.listen(port, () => console.log("My INTEX website is listening"));
