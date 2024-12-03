86d8544c556d68b475d9b1fff244ae9e6a9e080a
Gavin Smith <gavinjacobsmith@gmail.com>
2024-12-02 20:07:08 -0700

    intex is born

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+let app = express();
+
+let path = require("path");
+
+const port = 3000; 
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: true}));
+
+const knex = require("knex")({
+    client: "pg", 
+    connection: {
+        host: process.env.RDS_HOSTNAME || "localhost", 
+        user: process.env.RDS_USERNAME || "postgres", 
+        password: process.env.RDS_PASSWORD || "Gavin12", 
+        database: process.env.RDS_DB_NAME || "bucket_list", 
+        port: process.env.RDS_PORT || 5432, 
+    }
+});
+
+app.use(express.static(path.join(__dirname, "public") ))
+
+// Route to serve the landing page
+app.get('/', (req, res) => {
+  res.render('index')
+});
+
+app.listen(port, () => console.log("My travel website is listening"));
