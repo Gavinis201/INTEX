56710018c3be6a8db0250bb0720549e0d9b0d1e2
Jordan Goddard <137590372+jordangoddard6@users.noreply.github.com>
2024-12-04 00:59:45 -0700

    Add view_event.ejs functionality (#8)
    * Test
    
    * Removed test
    
    * Created view_events view
    
    * Add nav header in html body to view_events view
    
    * Temporary changes to index.js. Changed database connection to access on local machine. Added post route from login view to show view_events view
    
    * Added footer to view_events
    
    * Changed password error and added knex statement to pull data for ejs
    
    * Added table and ejs that pulls from knex request in index.js
    
    * Added code to format date coming from first_choice_event_date column
    
    * Now displays second and third choice dates
    
    * Changed table code to cards
    
    * changed cards to span the width of the page
    
    * Changed cards to expand when clicked
    
    * Added animation to open and closing of the cards
    
    * Added first_choice_event_date and event_status to closed card
    
    * Changed color of event_status depending on the status and right aligned the event status on the closed card
    
    * Moved first choice date and location on closed card below name in smaller font size
    
    * Better padding between dates header and dates list
    
    * Add all event information to expanded card
    
    * Formatted start time
    
    * Made 3 columns in the expanded card
    
    * Fixed spacing between Preferred Dates text and ordered list beneath
    
    * Fix h1 tag spacing
    
    * Moved information around in the columns and added a contact button
    
    * Added contact email to event_requests table and made phone numbers look pretty
    
    * Changed database connection and added email to third column
    
    * Third column adjustments
    
    * Added hortizontal rule and fixed vertical spacing around it
    
    * Set a route for my code in index.js to prepare for merging back to main. Also added comments

--- index.js
+++ index.js
@@ -15,14 +15,36 @@ const knex = require("knex")({
     client: "pg", 
     connection: {
         host: process.env.RDS_HOSTNAME || "localhost", 
-        user: process.env.RDS_USERNAME || "postgres", 
-        password: process.env.RDS_PASSWORD || "Gavin12", 
-        database: process.env.RDS_DB_NAME || "TurtleShelterProject", 
+        user: process.env.RDS_USERNAME || "intex", 
+        password: process.env.RDS_PASSWORD || "password", 
+        database: process.env.RDS_DB_NAME || "intex_test13", 
         port: process.env.RDS_PORT || 5432, 
     }
 });
 
-app.use(express.static(path.join(__dirname, "public") ))
+app.use(express.static(path.join(__dirname, "public") ));
+
+// This formats time so that if it's stored as 09:00:00, we can display it as 9 AM
+const formattedTime = (time) => {
+  // Convert "09:00:00" to a Date object
+  const date = new Date(`1970-01-01T${time}Z`);  // Use a dummy date
+
+  // Get the hour in 24-hour format
+  let hour = date.getUTCHours();
+
+  // Determine AM or PM
+  const period = hour >= 12 ? 'PM' : 'AM';
+
+  // Convert hour to 12-hour format
+  if (hour > 12) {
+    hour -= 12;
+  } else if (hour === 0) {
+    hour = 12; // Midnight case
+  }
+
+  // Return formatted time like "9 AM"
+  return `${hour} ${period}`;
+};
 
 // Route to serve the landing page
 app.get('/', (req, res) => {
@@ -32,6 +54,42 @@ app.get('/', (req, res) => {
 // Route to serve the login page
 app.get('/loginPage', (req, res) => {
   res.render('loginPage')
+});
+
+// view_events page code
+app.post('/view_events', (req, res) => {
+  knex.select('*')
+  .from('event_requests')
+  .join('event_status', 'event_requests.event_status_id','=','event_status.event_status_id' )
+  .join('space_size', 'event_requests.space_size_id','=','space_size.space_size_id' )
+  .join('event_type', 'event_requests.event_type_id','=','event_type.event_type_id' )
+  .orderBy('first_choice_event_date')
+  .then( event_requests => {
+      // Format the dates in the event_requests array
+      event_requests = event_requests.map(event => {
+        if (event.first_choice_event_date) { 
+          const date = new Date(event.first_choice_event_date);
+          event.first_choice_event_date = date.toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'}); // Formats as "January 10, 2024"
+        }
+        if (event.second_choice_event_date) { 
+          const date = new Date(event.second_choice_event_date);
+          event.second_choice_event_date = date.toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'}); // Formats as "January 10, 2024"
+        }
+        if (event.third_choice_event_date) { 
+          const date = new Date(event.third_choice_event_date);
+          event.third_choice_event_date = date.toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'}); // Formats as "January 10, 2024"
+        }
+        return event;
+      });
+      // Format the time for each event request
+      event_requests.forEach(event => {
+        event.formatted_time = formattedTime(event.estimated_event_start_time);
+      });
+    res.render("view_events", { event_requests });
+  }).catch(err => {
+      console.log(err);
+      res.status(500).json({err});
+    })
 });
 
 app.get("/volunteer", (req, res) => {
@@ -63,11 +121,5 @@ app.get("/volunteer", (req, res) => {
           res.status(500).send("Internal Server Error");
       });
 });
-
-
-
-
-
-
 
 app.listen(port, () => console.log("My INTEX website is listening"));
